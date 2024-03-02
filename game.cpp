c3094828cb3bb795a8c581f104e4ffff3262b4eb
Moses Makaka <moseskax@gmail.com>
2024-03-02 22:09:52 +0300

    Major adjustments

--- game.cpp
+++ game.cpp
@@ -27,7 +27,8 @@ void Setup()
 
 void Draw()
 {
-    system("clear"); // Use "clear" on Unix-based systems
+    system("clear"); // Use "clear" instead of "cls" for Unix-based systems
+
     for (int i = 0; i < width + 2; i++)
         cout << "#";
     cout << endl;
@@ -65,3 +66,101 @@ void Draw()
 
     for (int i = 0; i < width + 2; i++)
         cout << "#";
+    cout << endl;
+
+    cout << "Score:" << score << endl;
+}
+
+void Input()
+{
+    if (kbhit()) // Use kbhit instead of _kbhit for Unix-based systems
+    {
+        switch (getch()) // Use getch instead of _getch for Unix-based systems
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void Algorithm()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+
+    if (x >= width) x = 0;
+    else if (x < 0) x = width - 1;
+
+    if (y >= height) y = 0;
+    else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Algorithm();
+        sleep(1); // Adjust the sleep time as needed
+    }
+
+    return 0;
+}
